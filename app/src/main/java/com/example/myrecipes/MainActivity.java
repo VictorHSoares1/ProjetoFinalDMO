1fa60d6cf27c0174e05459d01b0083a1b9e05127
vitor <hugo.soares@aluno.ifsp.edu.br>
2022-12-04 11:55:53 -0300

    Primeira Parte do projeto

--- app/src/main/java/com/example/myrecipes/MainActivity.java
+++ app/src/main/java/com/example/myrecipes/MainActivity.java
@@ -1,14 +1,84 @@
 package com.example.myrecipes;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private TextView txtTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        txtTitle = findViewById(R.id.toolbar_title);
+        txtTitle.setText(getString(R.string.app_name));
+
+        drawerLayout = findViewById(R.id.nav_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.toggle_open, R.string.toggle_close);
+        drawerLayout.addDrawerListener(toggle);
+
+        toggle.syncState();
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent intent = null;
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        intent = new Intent(MainActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.nav_account:
+                        Toast.makeText(MainActivity.this, "Minha Conta", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_register_salty:
+                        intent = new Intent(MainActivity.this,SaltyRecipesRegisterActivity.class);
+                        startActivity(intent);
+                        break;
+                    //case R.id.nav_ranking:
+                        //Toast.makeText(MainActivity.this, "Classificação", Toast.LENGTH_SHORT).show();
+                       // break;
+                    //case R.id.nav_statitics:
+                       // Toast.makeText(MainActivity.this, "Estatísticas", Toast.LENGTH_SHORT).show();
+                       // break;
+                    case R.id.nav_logout:
+                        Toast.makeText(MainActivity.this, "Sair", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
     }
 }
--- app/src/main/java/com/example/myrecipes/SaltyRecipesRegisterActivity.java
+++ app/src/main/java/com/example/myrecipes/SaltyRecipesRegisterActivity.java
@@ -1,14 +1,34 @@
 package com.example.myrecipes;
 
-import androidx.appcompat.app.AppCompatActivity;
+import  androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SaltyRecipesRegisterActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TextView txtTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_salty_recipes_register);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        txtTitle = findViewById(R.id.toolbar_title);
+        txtTitle.setText("Nova Atividade");
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
